4af4753af764d40a79ad8beaa5646d266fb82c0d
nawab69 <nawabkhairuzzaman@gmail.com>
2021-04-18 21:37:14 +0600

    content / text /image/ footer/ header / favicon added and updated

--- frontend/js/calculate-script.js
+++ frontend/js/calculate-script.js
@@ -1,125 +1,46 @@
+"use strict";
 
-const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+const price = document.getElementById("price-1");
+
+const convertMoney = (labelValue) => {
+  // Nine Zeroes for Billions
+  return Math.abs(Number(labelValue)) >= 1.0e9
+    ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + "B"
+    : // Six Zeroes for Millions
+    Math.abs(Number(labelValue)) >= 1.0e6
+    ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + "M"
+    : // Three Zeroes for Thousands
+    Math.abs(Number(labelValue)) >= 1.0e3
+    ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + "K"
+    : Math.abs(Number(labelValue));
 };
 
-const usdInput = document.getElementById('amtUSD');
-const btcInput = document.getElementById('amtBTC');
-let amtBTC = btcInput.value;
-const CoinInput = document.getElementById("selected-coin");
-const CurrencyInput = document.getElementById("selected-currency");
-let coinName = CoinInput.options[CoinInput.selectedIndex].value;
-let currencyName = CurrencyInput.options[CurrencyInput.selectedIndex].value;
-let cryptoRate = {
-    bitcoin: 0,
-    ethereum: 0,
-    ripple: 0,
-    monero: 0,
-    litecoin: 0,
-    "bitcoin-cash": 0,
+const executePrice = (data) => {
+  for (const [key, value] of Object.entries(data)) {
+    const price = document.getElementById(`price-${key}`);
+    const market = document.getElementById(`market-${key}`);
+    const supply = document.getElementById(`supply-${key}`);
+    const volume = document.getElementById(`volume-${key}`);
+    const change = document.getElementById(`change-${key}`);
+
+    price.innerText = `$${parseFloat(value.quote.USD.price).toFixed(2)}`;
+    market.innerText = `$${convertMoney(value.quote.USD.market_cap)}`;
+    supply.innerText = `$${convertMoney(value.total_supply)}`;
+    volume.innerText = `$${convertMoney(value.quote.USD.volume_24h)}`;
+    change.innerText = `${parseFloat(
+      value.quote.USD.percent_change_24h
+    ).toFixed(2)}`;
+
+    if (value.quote.USD.percent_change_24h < 0) {
+      change.classList.add("red");
+    } else {
+      change.classList.add("green");
+    }
+  }
 };
-let fiat_currency_rates = {};
 
-function bitconFetchCoinRate(callback) {
-    // build rate array on page load 
-    fetch("https://api.coincap.io/v2/assets?ids=bitcoin,ethereum,ripple,bitcoin-cash,litecoin,monero", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            const r = JSON.parse(result);
-            // build rate array
-            if (r.data) {
-
-                r.data.forEach(crypto => {
-                    cryptoRate[crypto.id] = parseFloat(crypto.priceUsd);
-                });
-
-                bitcoinFetchCurrencyRate(callback);
-                
-            }
-        })
-        .catch(error => console.log('error', error));
-}
-
-// fetch rate and set inital exchange rate
-bitconFetchCoinRate(bitconCalcualteCoinRate);
-
-// change exchange rate on crypotocurrency amount change
-$('#amtBTC').bind('change paste keyup', function() {
-    coinName = CoinInput.options[CoinInput.selectedIndex].value;
-    currencyName = CurrencyInput.options[CurrencyInput.selectedIndex].value;
-
-    const coinAmount = $(this).val();
-    bitconCalcualteCoinRate(coinName, coinAmount, currencyName);
-});
-
-
-// change exchange rate on crypotocurrency name change
-function bitconCalcualteCoinRate(coinName, coinAmount, currencyName) {
-
-    const totalUSD = coinAmount * cryptoRate[coinName];
-    let totalValue = totalUSD;
-
-    if ('USD' !== currencyName) {
-        let currencyValue = fiat_currency_rates[currencyName];
-
-        totalValue = totalUSD * currencyValue;
-    }
-
-
-    usdInput.value = totalValue.toFixed(2);
-}
-
-
-function bitcoinFetchCurrencyRate(callback) {
-    fetch("https://api.coincap.io/v2/rates", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            let r = JSON.parse(result);
-
-            let selectOptions = '';
-            r.data.forEach((value, index) => {
-                //console.log(value);
-                if ("fiat" === value.type) {
-                    let rate1usd = parseFloat(value.rateUsd);
-                    if (rate1usd <= 1) {
-                        rate1usd = 1 / rate1usd;
-                        fiat_currency_rates[value.symbol] = rate1usd;
-                    } else {
-                        fiat_currency_rates[value.symbol] = rate1usd;
-                    }
-
-                    //selectOptions += `<option value="${value.symbol}">${value.symbol} - ` + titleCase(value.id.replace("-", " ")) + '</option>';
-                }
-            });
-            //console.log(selectOptions);
-            //CurrencyInput.innerHTML = selectOptions;
-            callback(coinName, amtBTC, currencyName); // set the btc rate on first page load
-            return fiat_currency_rates;
-
-        })
-        .catch(error => console.log('error', error));
-}
-
-function bitconCalculateOnCryptoChange(coinName){
-    amtBTC= btcInput.value;
-    currencyName = CurrencyInput.options[CurrencyInput.selectedIndex].value;
-    bitconCalcualteCoinRate(coinName, amtBTC, currencyName);
-}
-
-function bitconCalculateOnCurrencyChange(currencyName) {
-    amtBTC = btcInput.value;
-    coinName = CoinInput.options[CoinInput.selectedIndex].value;
-    bitconCalcualteCoinRate(coinName, amtBTC, currencyName);
-}
-
-function titleCase(str) {
-    var splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    // Directly return the joined string
-    return splitStr.join(' ');
-}
+fetch("api/prices")
+  .then((response) => response.json())
+  .then((data) => {
+    executePrice(data);
+  });

fc14816ab9f0fccfd851118c95b59a547adb2fde
nawab69 <nawabkhairuzzaman@gmail.com>
2021-04-19 00:16:50 +0600

    cron job added

--- api/server.js
+++ api/server.js
@@ -6,9 +6,35 @@ import userRoutes from "./routes/userRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 import path from "path";
 import priceRoutes from "./routes/priceRoutes.js";
+import cron from "node-cron";
+import axios from "axios";
+import fs from "fs";
 
 dotenv.config();
 const app = express();
+
+const writeData = (data) => {
+  fs.writeFile("./data/price.json", data, () => {
+    console.log("file written");
+  });
+};
+
+cron.schedule("*/8 * * * *", function () {
+  axios
+    .get(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=bitcoin,ethereum,litecoin,tether,xrp,cardano,bitcoin-cash,stellar,chainlink,monero&aux=total_supply",
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": "801f6b48-1c04-4af7-8518-29d7dc2d2082",
+        },
+      }
+    )
+    .then(function (response) {
+      let data = response.data;
+      writeData(JSON.stringify(data));
+    })
+    .catch((e) => console.log(e));
+});
 
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), "../frontend")));
